a7e599d257c1468bd5feff25b71d400beeb7de99
Nicholas Orndorff <norndorff@live.com>
2022-09-28 14:47:46 -0700

    motor sizing data file

--- motor.py
+++ motor.py
@@ -1,1 +1,16 @@
+import csdl
+import python_csdl_backend
+
+class mach(csdl.Model):
+    def initialize(self):
+        pass
+    def define(self):
+
+        power_density = self.declare_variable('power_density') # (kw/kg) ~3-5
+        available_power = self.declare_variable('available_power')
+
+        motor_weight = available_power/power_density
+
+        self.register_output('motor_weight',motor_weight)
+
 
