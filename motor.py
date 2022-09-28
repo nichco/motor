ffd9517cf1b5740f80c3f0d65fc8a67288c6b88b
Nicholas Orndorff <norndorff@live.com>
2022-09-28 16:40:06 -0700

    power density in watts rather than kw

--- motor.py
+++ motor.py
@@ -6,7 +6,7 @@ class mach(csdl.Model):
         pass
     def define(self):
 
-        power_density = self.declare_variable('power_density') # (kw/kg) ~3-5
+        power_density = self.declare_variable('power_density') # (w/kg) ~3000-5000
         available_power = self.declare_variable('available_power')
 
         motor_weight = available_power/power_density
